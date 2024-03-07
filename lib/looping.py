437f7969b8e0b38ab068858711543e5147df1fe0
Soojungkim <soojung.kimm@gmail.com>
2024-03-07 13:36:54 -0800

    lab complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i > 0:
+        print (f'{i}')
+        i -= 1
+        print('Happy New Year!')
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    return [num ** 2 for num in int_list]
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for num in range(1, 101):
+        if(num % 3 == 0 and num % 5 == 0):
+            print('FizzBuzz')
+        elif(num % 3 == 0):
+            print('Fizz') 
+        elif(num % 5 == 0):
+            print('Buzz') 
+        else:
+            print(num)
     pass
